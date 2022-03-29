597af9d917b1814164399276f8fa4b96ad620d99
Prerit Dayal <prerit714@gmail.com>
2022-03-29 09:54:57 +0530

    Creating component and doing changes in module files without generate commands

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Import my custom courses component
+import { CoursesComponent } from './courses.component';
+
+// This is a way to convert a normal class to a ngModule
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // Add to declarations array after importing your custom component
+    CoursesComponent, // If you forget this entry, you will get errors
+    // You will get an error like "courses" is not a known element
   ],
   imports: [
     BrowserModule,
---
+++ src/app/courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  // The idea is to define custom html-elements
+  selector: "courses", // courses will be the selector name
+  template: `
+    <h2> Courses </h2>
+  `,
+})
+export class CoursesComponent {
+  // Leaving this class empty for now 
+}
