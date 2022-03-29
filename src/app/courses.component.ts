e8d3fba8bf22d84e1284821c9fa4e535e5d6a29f
Prerit Dayal <prerit714@gmail.com>
2022-03-29 10:10:27 +0530

    Using interpolation to call a method

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,10 +3,19 @@ import { Component } from "@angular/core";
 @Component({
   // The idea is to define custom html-elements
   selector: "courses", // courses will be the selector name
+  // Using data-binding and interpolation syntax ({{}} syntax), whenever the value
+  // of title changes, the component is re-rendered
+  // Any valid JS expression is allowed between {{}}
   template: `
-    <h2> Courses </h2>
+    <h2>{{ "Titiles are: " + getTitle() }}</h2>
   `,
 })
 export class CoursesComponent {
   // Leaving this class empty for now 
+  // Define a field "title"
+  title: string = "List of courses";
+
+  getTitle(): string {
+    return this.title;
+  }
 }
