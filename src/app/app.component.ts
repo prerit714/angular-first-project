326b2e05f1e00fe4f7edbe6151700fdf094e383e
Prerit Dayal <prerit714@gmail.com>
2022-03-29 23:10:37 +0530

    One way of making a component accept input data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  post = {
+    title: "Hello World!",
+    isFavorite: true
+  };
   title = 'angular-first-project';
 }
--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+// import { Input } from "@angular/core";
 
 @Component({
+  // This object is also sometimes referred to as meta-data
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
+  // Second way to declare a property as input property is
+  inputs: [
+    // A better approach, but has a problem!
+    "isFavorite"
+  ],
 })
 export class FavoriteComponent implements OnInit {
 
+  // Task is to mark fields as input properties/output properties
+  // We use Input decorator to make a field as an input property
+  // @Input() isFavorite: boolean = false;
   isFavorite: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
