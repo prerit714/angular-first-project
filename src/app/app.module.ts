bd98b991bf499005914989095eba55c5480a449a
Prerit Dayal <prerit714@gmail.com>
2022-03-29 10:00:52 +0530

    Added a course component using ng g c course command

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { AppComponent } from './app.component';
 
 // Import my custom courses component
 import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
 
 // This is a way to convert a normal class to a ngModule
 @NgModule({
   declarations: [
     AppComponent,
     // Add to declarations array after importing your custom component
-    CoursesComponent, // If you forget this entry, you will get errors
-    // You will get an error like "courses" is not a known element
+    CoursesComponent, // don't forget to declare a component, if you add one
+    // without using the ng-generate command, as this will generate an error
+    CourseComponent, // This line of code was added by "ng g c course" command
   ],
   imports: [
     BrowserModule,
