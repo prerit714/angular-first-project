33f6e185936fb601f6edfeff7a4e5680c907db35
Prerit Dayal <prerit714@gmail.com>
2022-03-30 11:30:44 +0530

    Using trackBy to improve performace? in case of lag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,7 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
 export class AppComponent {
   // courses = [1, 2, 3, 4, 5]; // To show/hide part of a page, using a simple
   // array, in real world example this will be an array of course objects
-  courses = [
-    {id: 1, name: "Physics"},
-    {id: 2, name: "Chemistry"},
-    {id: 3, name: "Biology"},
-    {id: 4, name: "Mathematics"},
-  ];
+  courses: any;
   tweet = {
     body: "Hello I am Prerit Dayal!",
     likesCount: 20,
@@ -46,5 +41,28 @@ export class AppComponent {
     const indexOfCourseToDelete = this.courses.indexOf(course);
     this.courses.splice(indexOfCourseToDelete, 1);
     // THIS WORKS!!!
+  }
+  loadCourses(): void {
+    // This method will initialize the courses
+    this.courses = [
+      // Will be memory intensive as angular will re-render the dom 
+      // on every LoadCourses button click, to avoid that I will use
+      // trackBy exported property
+      // very similar to useMemo of react!
+      // using trackBy will let angular to compare the objects received
+      // using content instead of memory location (kinda memory intensive)
+      // But is atleast a better optimization when compared to taking extra time
+      {id: 1, name: "Physics"},
+      {id: 2, name: "Chemistry"},
+      {id: 3, name: "Biology"},
+      {id: 4, name: "Mathematics"},
+    ];
+  }
+  trackCourse(_index: number, course: any): void {
+    // This function is what which is responsible for trackBy: trackCourse
+    return course ? course.id: undefined;
+    // If you are dealing with large list and you are experiencing performace
+    // problems ONLY then use this trackBy: trackCourse. Since it is a performace
+    // OVERHEAD
   }
 }
